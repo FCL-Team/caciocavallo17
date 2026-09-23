8778f73e774e6424b2fecd1b29a9f9f422a841ab
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-09-23 22:05:03 +0800

    build: 构建 CI 与 Maven 模块调整，Java 8 字节码降级流程

--- build_and_out.sh
+++ build_and_out.sh
@@ -5,4 +5,16 @@ mvn -B package --file pom.xml -DskipTests
 
 rm -rf out
 mkdir -p out
-cp cacio-*/target/cacio-*-1.19-SNAPSHOT.jar out
+cp cacio-*/target/cacio-*-1.19.1-SNAPSHOT.jar out
+mv out/cacio-agent-1.19.1-SNAPSHOT.jar out/cacio-agent.jar
+
+# 下载专门的字节码工具
+if [ ! -f "JavaDowngrader-Standalone-1.1.2.jar" ]; then
+    wget -q https://github.com/RaphiMC/JavaDowngrader/releases/download/v1.1.2/JavaDowngrader-Standalone-1.1.2.jar
+fi
+
+for jarfile in out/cacio-*.jar; do
+        echo "Downgrading: $(basename $jarfile)"
+        java -jar JavaDowngrader-Standalone-1.1.2.jar -v 8 -i "$jarfile" -o "$jarfile.tmp"
+        mv "$jarfile.tmp" "$jarfile"
+done
